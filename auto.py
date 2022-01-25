ba417a96b5481f6af62f8854fdc8487ff8dd1536
Redwan Diriye <ridwanmohamedco@outlook.com>
2022-01-24 21:50:09 -0600

    test

---
+++ auto.py
@@ -0,0 +1,11 @@
+from email import message
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+
+subject = "Email From Python"
+body = "This is a test email from Python"
+sender = "virus.run.py@gmail.com"
+receiver = "virus.run.py@gmail.com"
+

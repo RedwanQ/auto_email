48178df3055e1173c1c4bd169e267edd70584df4
Redwan Diriye <ridwanmohamedco@outlook.com>
2022-01-24 22:06:31 -0600

    main-test

--- auto.py
+++ auto.py
@@ -1,4 +1,5 @@
 from email import message
+from http import server
 import smtplib
 import ssl
 from email.message import EmailMessage
@@ -8,4 +9,31 @@ subject = "Email From Python"
 body = "This is a test email from Python"
 sender = "virus.run.py@gmail.com"
 receiver = "virus.run.py@gmail.com"
+password = input("Enter password: ")
 
+message = EmailMessage()
+message["From"] = sender
+message["To"] = receiver
+message["Subject"] = subject
+
+
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
+message.add_alternative(html, subtype="html")
+
+context = ssl.create_default_context()
+
+print("Sending.....")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message.as_string())
+
+print("Succes!")
